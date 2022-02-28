a863be95a13ea465fb8cc282a720da9f225ec323
Rk Rakib hasan <rkrakibhasan680@gmail.com>
2022-02-28 23:33:34 +0600

    add some js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("Hello ");
+const searchPhone=()=>{
+    const searchField= document.getElementById("search-input");
+    const searchText=searchField.value;
+    searchField.value="";
+    const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    console.log(url);
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>displayPhoneResult(data.data));
+};
+
+const displayPhoneResult=(phones)=>{
+    const cardId=document.getElementById("card-area");
+phones.forEach((phone)=>{  
+    console.log(phone); 
+    const div=document.createElement("div");
+    div.innerHTML=`
+    <div class="col" onClick="PhoneInfomation('${phone.slug}')">
+      <div class="card">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <p class="card-text">${phone.brand.slice(0,20)}</p>
+        </div>
+      </div>
+    </div>
+    `;
+    cardId.appendChild(div);
+
+});
+
+};
