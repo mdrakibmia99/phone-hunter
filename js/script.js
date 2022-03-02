08c3dbe6069cfd321fe392ddf742f762b222b733
Rk Rakib hasan <rkrakibhasan680@gmail.com>
2022-03-02 19:53:42 +0600

    change something 123

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-// get for Phnone name using serach
+
+// global variable
+const cardId = document.getElementById("card-area");
+// get  Phnone name using serach and connect with server
 const searchPhone = () => {
   const searchField = document.getElementById("search-input");
+  cardId.textContent="";
   const searchText = searchField.value;
   searchField.value = "";
   document.getElementById("phoneDetails").textContent = "";
-
-  // spin start
+// spin start
   const bookLoading = document.getElementById("spinner");
   bookLoading.innerHTML = `
       <div class="spinner-border text-info m-auto" role="status">
           <span class="visually-hidden">Loading...</span>
       </div>
     `;
+ // spin end
 
+//  api connection
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   fetch(url)
     .then((response) => response.json())
@@ -25,22 +30,29 @@ const searchPhone = () => {
       );
     });
 };
-// display all phones
+
+
+// display all phones start
 const displayPhoneResult = (phones, totalPhone, customlength, searchText) => {
   if (searchText == "") {
-    // spiner off
+
+// spiner off
     const showTitle = document.getElementById("spinner");
     showTitle.innerHTML = "";
+
+//if input is null then show this error messeage
     document.getElementById("phone_not_avabile").innerText =
       "Please enter a phone name!!";
+
   } else {
+
     document.getElementById("showing").innerHTML = `
   <div class="text-center text-success">
     <span class="fw-bold">Total Phone Found:</span> ${totalPhone},
     <span class="fw-bold">Showing Results:</span> ${customlength.length}
   </div>`;
 
-    const cardId = document.getElementById("card-area");
+    
     const PhoneNotAvaileId = document.getElementById("phone_not_avabile");
     PhoneNotAvaileId.textContent = "";
     cardId.textContent = "";
@@ -48,15 +60,18 @@ const displayPhoneResult = (phones, totalPhone, customlength, searchText) => {
     if (phones.length === 0) {
       PhoneNotAvaileId.innerText = "Result Not Founded!!";
     } else {
+      cardId.textContent = "";
       customlength.forEach((phone) => {
+//for  Create a card
         const div = document.createElement("div");
         div.innerHTML = `
-      <div class="col " onClick="PhoneInfomation('${phone.slug}')">
-        <div class="card ">
+      <div class="col " >
+        <div class="card rounded shadow ">
           <img src="${phone.image}" class="card-img-top w-75 mx-auto" alt="...">
           <div class="card-body text-center">
             <h5 class="card-title fw-bold">${phone.phone_name}</h5>
             <p class="card-text">Brand:${phone.brand}</p>
+            <a href="#" onclick="PhoneInfomation('${phone.slug}')" class="btn btn-primary mx-auto">Show Details</a>
           </div>
         </div>
       </div>
@@ -69,57 +84,55 @@ const displayPhoneResult = (phones, totalPhone, customlength, searchText) => {
     showTitle.innerHTML = "";
   }
 };
-// phone information api get by phone Id
+// display all phones end
+
+
+// phone information api get by phone Id start
 const PhoneInfomation = (id) => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => displayPhoneDetails(data.data));
 };
-// for display phone full informaiton
-const displayPhoneDetails = (phone) => {
-  const cardId = document.getElementById("phoneDetails");
-  cardId.textContent = "";
-  const div = document.createElement("div");
+// phone information api get by phone Id end
 
-  div.innerHTML = `
-<div class="card ">
-<img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
-<div class="card-body">
-<h5 class="card-title fw-bold">${phone.name}</h5>
-<p class="card-text"> ${phone.releaseDate}</p>
-<p class="card-text">Brand:${phone.brand}</p>
 
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    More Details..
-  </button>
-  
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">${phone.name}</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
+// for display phone full informaiton start 
+const displayPhoneDetails = (details) => {
+  const detailSection = document.getElementById('phoneDetails');
+  detailSection.textContent = '';
+  const detailDiv = document.createElement('div');
+  detailDiv.innerHTML =  `
     
-         <p> Chip Set: ${phone.mainFeatures.chipSet} </p>
-         <p> Display Size: ${phone.mainFeatures.displaySize} </p>
-         <p> Memory: ${phone.mainFeatures.memory} </p>
-         <p> Sensors: ${phone.mainFeatures.sensors} </p>
-         <p> Storage: ${phone.mainFeatures.storage} </p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        </div>
+  <div class="product-detail rounded shadow">
+      <div class="product-detail-left">
+          <img src="${details.image}" class="img-fluid">
+          <h4 class="text-primary">${details.name}</h4>
+          <p class="text-danger text-center">${details.releaseDate ? details.releaseDate : "No Release Date !!!"}</p>
       </div>
-    </div>
+
+      <div class="product-detail-middle">
+          <h5 ><b><u>Main Features:</u></b> </h5>
+          <p><b>Storage: </b><span>${details.mainFeatures.storage}</span></p>
+          <p><b>Display Size: </b><span>${details.mainFeatures.displaySize}</span></p>
+          <p><b>Chipset: </b><span>${details.mainFeatures.chipSet}</span></p>
+          <p><b>Memory: </b><span>${details.mainFeatures.memory}</span></p>
+          <p><b>Sensors: </b><span>${details.mainFeatures.sensors}</span></p>
+      </div>
+
+      <div class="product-detail-right">
+          <h5><b><u>Other Features:</u></b> </h5>
+          <p><b>WLAN: </b><span>${details.others ? details.others.WLAN : 'No Information!'}</span></p>
+          <p><b>Bluetooth: </b><span>${details.others ? details.others.Bluetooth : 'No Information !'}</span></p>
+          <p><b>GPS: </b><span>${details.others ? details.others.GPS : 'No Information !'}</span></p>
+          <p><b>NFC: </b><span>${details.others ? details.others.NFC : 'No Information !'}</span></p>
+          <p><b>Radio: </b><span>${details.others ? details.others.Radio : 'No Information !'}</span></p>
+          <p><b>USB: </b><span>${details.others ? details.others.USB : 'No Information !'}</span></p>
+      </div>
   </div>
+`
+  
+    
+  detailSection.appendChild(detailDiv);
 
-
-
-</div>
-</div>`;
-  cardId.appendChild(div);
-};
+}
